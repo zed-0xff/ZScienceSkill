d7f4e2006481c121a6abeb0d216a751160113f38
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-04 21:02:57 +0200

    upd

--- 42.13/media/lua/shared/ZScienceSkill_BookXP.lua
+++ 42.13/media/lua/shared/ZScienceSkill_BookXP.lua
@@ -7,12 +7,31 @@ require "ZScienceSkill_Data"
 
 -- SP: both are called, perform() -> complete()
 
+-- supposed to run on server only
+local function is_literature_read(playerObj, item)
+    if not item then return true end
+    if not item:IsLiterature() then return true end
+    -- next is a copy of ISInventoryPane:isLiteratureRead(playerObj, item)
+    local modData = item:hasModData() and item:getModData() or nil
+    if modData ~= nil then
+        if (modData.literatureTitle) and playerObj:isLiteratureRead(modData.literatureTitle) then return true end
+        if (modData.printMedia ~= nil) and playerObj:isPrintMediaRead(modData.printMedia.title) then return true end
+        if (modData.learnedRecipe ~= nil) and playerObj:getKnownRecipes():contains(modData.learnedRecipe) then return true end
+    end
+    local skillBook = SkillBook[item:getSkillTrained()]
+    if (skillBook ~= nil) and (item:getMaxLevelTrained() < playerObj:getPerkLevel(skillBook.perk) + 1) then return true end
+    if (item:getNumberOfPages() > 0) and (playerObj:getAlreadyReadPages(item:getFullType()) == item:getNumberOfPages()) then return true end
+    if (item:getLearnedRecipes() ~= nil) and playerObj:getKnownRecipes():containsAll(item:getLearnedRecipes()) then return true end
+    return false
+end
 
 local orig_complete = ISReadABook.complete
 function ISReadABook:complete()
-    local result = orig_complete(self)
+    if self.isLiteratureRead == nil then
+        self.isLiteratureRead = is_literature_read(self.character, self.item)
+    end
 
-    print("ISReadABook:complete", self.isLiteratureRead)
+    local result = orig_complete(self) -- saves literature read flags
 
     if not self.isLiteratureRead then
         local itemType = self.item:getFullType()
