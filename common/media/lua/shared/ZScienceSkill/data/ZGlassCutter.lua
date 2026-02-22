f703ce9cc35c13672cf272451b4424f4647ad2f2
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-22 11:32:37 +0200

    ZGlassCutter itegration

---
+++ common/media/lua/shared/ZScienceSkill/data/ZGlassCutter.lua
@@ -0,0 +1,10 @@
+require "ZScienceSkill/Data"
+
+ZScienceSkill.Data.add({
+    specimens = {
+        ["ZGlassCutter.GlassCutter"] = { Science: 10, Maintenance: 10, Glassmaking: 10 },
+    },
+    literatureReadOnce = {
+        ["ZGlassCutter.GlassCuttingMag"] = 10,
+    },
+})
