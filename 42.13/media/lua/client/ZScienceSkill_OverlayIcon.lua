da1dee860c351ae42c0742acb376e21ae6270102
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-22 20:49:24 +0200

    use zsHook

--- 42.13/media/lua/client/ZScienceSkill_OverlayIcon.lua
+++ 42.13/media/lua/client/ZScienceSkill_OverlayIcon.lua
@@ -3,6 +3,7 @@
 -- Green tick: researched specimens (optional)
 require "ZScienceSkill/Data"
 require "ZScienceSkill_ModOptions"
+require 'zsHook'
 
 local grayTickTexture = getTexture("media/ui/R_Mark_Gray.png")
 local greenTickTexture = getTexture("media/ui/Tick_Mark-10.png")
@@ -23,7 +24,6 @@ local function isSpecimenResearched(player, fullType)
     return modData[researchKey]
 end
 
-
 local function isFluidResearched(player, fluidType)
     local modData = player:getModData().researchedSpecimens
     return modData and modData["Fluid:" .. fluidType]
@@ -34,82 +34,82 @@ local function isLiteratureReadOnce(player, fullType)
     return modData and modData[fullType]
 end
 
-local originalRenderDetails = ISInventoryPane.renderdetails
+zsHook( ISInventoryPane, {
+    renderdetails = function(orig, self, doDragged, ...)
+        orig(self, doDragged, ...)
 
-function ISInventoryPane:renderdetails(doDragged)
-    originalRenderDetails(self, doDragged)
+        if not doDragged then return end
 
-    if not doDragged then return end
+        local player = getSpecificPlayer(self.player)
+        if not player then return end
 
-    local player = getSpecificPlayer(self.player)
-    if not player then return end
+        local showOverlay = ZScienceSkillOptions.isOverlayEnabled()
+        local showCheckmark = ZScienceSkillOptions.isResearchedCheckmarkEnabled()
+        
+        if not showOverlay and not showCheckmark then return end
 
-    local showOverlay = ZScienceSkillOptions.isOverlayEnabled()
-    local showCheckmark = ZScienceSkillOptions.isResearchedCheckmarkEnabled()
-    
-    if not showOverlay and not showCheckmark then return end
+        local YSCROLL = self:getYScroll()
+        local HEIGHT = self:getHeight()
 
-    local YSCROLL = self:getYScroll()
-    local HEIGHT = self:getHeight()
-
-    local y = 0
-    for _, v in ipairs(self.items) do
-        local item = v.items and v.items[1] or v
-        if item and type(item.getFullType) == "function" then
-            local fullType = item:getFullType()
-            local texture = nil
-            
-            -- Science literature: gray "R" if unread
-            if ZScienceSkill.Data.literature[fullType] then
-                if not self:isLiteratureRead(player, item) and showOverlay then
-                    texture = grayTickTexture
-                end
-            -- Read-once literature: gray "R" if unread, green tick if read
-            elseif ZScienceSkill.Data.literatureReadOnce[fullType] then
-                if isLiteratureReadOnce(player, fullType) then
-                    if showCheckmark then
-                        texture = greenTickTexture
+        local y = 0
+        for _, v in ipairs(self.items) do
+            local item = v.items and v.items[1] or v
+            if item and type(item.getFullType) == "function" then
+                local fullType = item:getFullType()
+                local texture = nil
+                
+                -- Science literature: gray "R" if unread
+                if ZScienceSkill.Data.literature[fullType] then
+                    if not self:isLiteratureRead(player, item) and showOverlay then
+                        texture = grayTickTexture
                     end
-                elseif showOverlay then
-                    texture = grayTickTexture
-                end
-            -- Specimens: gray "R" if unresearched, green tick if researched
-            elseif ZScienceSkill.Data.specimens[fullType] then
-                if isSpecimenResearched(player, fullType) then
-                    if showCheckmark then
-                        texture = greenTickTexture
-                    end
-                elseif showOverlay then
-                    texture = grayTickTexture
-                end
-            else
-                -- Fluids: check if container has researchable fluid
-                local fluidType = ZScienceSkill.getFluidType(item)
-                if fluidType and ZScienceSkill.Data.fluids and ZScienceSkill.Data.fluids[fluidType] then
-                    if isFluidResearched(player, fluidType) then
+                -- Read-once literature: gray "R" if unread, green tick if read
+                elseif ZScienceSkill.Data.literatureReadOnce[fullType] then
+                    if isLiteratureReadOnce(player, fullType) then
                         if showCheckmark then
                             texture = greenTickTexture
                         end
                     elseif showOverlay then
                         texture = grayTickTexture
                     end
+                -- Specimens: gray "R" if unresearched, green tick if researched
+                elseif ZScienceSkill.Data.specimens[fullType] then
+                    if isSpecimenResearched(player, fullType) then
+                        if showCheckmark then
+                            texture = greenTickTexture
+                        end
+                    elseif showOverlay then
+                        texture = grayTickTexture
+                    end
+                else
+                    -- Fluids: check if container has researchable fluid
+                    local fluidType = ZScienceSkill.getFluidType(item)
+                    if fluidType and ZScienceSkill.Data.fluids and ZScienceSkill.Data.fluids[fluidType] then
+                        if isFluidResearched(player, fluidType) then
+                            if showCheckmark then
+                                texture = greenTickTexture
+                            end
+                        elseif showOverlay then
+                            texture = grayTickTexture
+                        end
+                    end
                 end
-            end
-            
-            if texture then
-                local topOfItem = y * self.itemHgt + YSCROLL
-                if topOfItem + self.itemHgt >= 0 and topOfItem <= HEIGHT then
-                    local xoff = 0
-                    local yoff = 0
-                    local texWH = math.min(self.itemHgt - 2, 32)
-                    local auxDXY = self.itemHgt - (self.itemHgt - texWH) / 2 - 13
-                    local texOffsetY = (y * self.itemHgt) + (self.itemHgt - texWH) / 2 + self.headerHgt + yoff
-                    local texOffsetX = self.column2 - texWH - (self.itemHgt - texWH) / 2 + xoff
+                
+                if texture then
+                    local topOfItem = y * self.itemHgt + YSCROLL
+                    if topOfItem + self.itemHgt >= 0 and topOfItem <= HEIGHT then
+                        local xoff = 0
+                        local yoff = 0
+                        local texWH = math.min(self.itemHgt - 2, 32)
+                        local auxDXY = self.itemHgt - (self.itemHgt - texWH) / 2 - 13
+                        local texOffsetY = (y * self.itemHgt) + (self.itemHgt - texWH) / 2 + self.headerHgt + yoff
+                        local texOffsetX = self.column2 - texWH - (self.itemHgt - texWH) / 2 + xoff
 
-                    self:drawTexture(texture, texOffsetX + auxDXY, texOffsetY + auxDXY - 1, 1, 1, 1, 1)
+                        self:drawTexture(texture, texOffsetX + auxDXY, texOffsetY + auxDXY - 1, 1, 1, 1, 1)
+                    end
                 end
             end
+            y = y + 1
         end
-        y = y + 1
-    end
-end
+    end -- renderdetails
+}) -- zsHook
