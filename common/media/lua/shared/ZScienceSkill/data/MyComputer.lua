a81d389f8c743d09c875e93aa5b9bfd16d64c43b
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-03-14 10:26:05 +0200

    upd

--- common/media/lua/shared/ZScienceSkill/data/MyComputer.lua
+++ common/media/lua/shared/ZScienceSkill/data/MyComputer.lua
@@ -1,5 +1,6 @@
 require "ZScienceSkill/Data"
 
 ZScienceSkill.Data.add({ specimens = {
-    ["MyComputer.ABHPonderPadLaptop"]  = { Science = 20, Electricity = 10, Hacking = 20 },
+    ["MyComputer.ABHPonderPadLaptop"]  = { Science = 20, Hacking = 20, Electricity = 10 },
+    ["MyComputer.FloppyDiskBinder"]    = { Science =  1, Hacking =  5 },
 }})
