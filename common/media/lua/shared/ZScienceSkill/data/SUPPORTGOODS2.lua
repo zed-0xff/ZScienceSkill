28f962600e50fefc2b10efc8d40e932f97261cce
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-03-31 21:47:22 +0300

    upd

--- common/media/lua/shared/ZScienceSkill/data/SUPPORTGOODS2.lua
+++ common/media/lua/shared/ZScienceSkill/data/SUPPORTGOODS2.lua
@@ -2,28 +2,29 @@ require "ZScienceSkill/Data"
 
 ZScienceSkill.Data.add({ specimens = {
     -- Electronics
-    ["SupportCorps.ABHCDDrive"]         = { Science =  5, Hacking =  5, Electricity =  5 },
-    ["SupportCorps.ABHMotherboard"]     = { Science = 15, Hacking = 15, Electricity = 10 },
-    ["SupportCorps.ABHPonderPadLaptop"] = { Science = 20, Hacking = 20, Electricity = 10 },
-    ["SupportCorps.ABHPrinter"]         = { Science = 10, Hacking =  5, Electricity =  5 },
-    ["SupportCorps.BatteryCharger"]     = { Science = 10, Electricity = 15 },
-    ["SupportCorps.FloppyDiskBinder"]   = { Science =  1, Hacking =  5 },
-    ["SupportCorps.InkCartridge"]       = { Science =  5, Hacking =  5 },
+    ["SupportCorps.ABHCDDrive"]             = { Science =  5, Hacking =  5, Electricity =  5 },
+    ["SupportCorps.ABHMotherboard"]         = { Science = 15, Hacking = 15, Electricity = 10 },
+    ["SupportCorps.ABHPonderPadLaptop"]     = { Science = 20, Hacking = 20, Electricity = 10 },
+    ["SupportCorps.ABHPrinter"]             = { Science = 10, Hacking =  5, Electricity =  5 },
+    ["SupportCorps.BatteryCharger"]         = { Science = 10, Electricity = 15 },
+    ["SupportCorps.FloppyDiskBinder"]       = { Science =  1, Hacking =  5 },
+    ["SupportCorps.InkCartridge"]           = { Science =  5, Hacking =  5 },
 
     -- Doctor
-    ["SupportCorps.ColdaFluPills"]      = { Science = 10, Doctor = 20 },
-    ["SupportCorps.OrthopedicCast"]     = { Science =  5, Doctor = 25 },
-    ["SupportCorps.PillCapsule"]        = { Science =  5, Doctor =  5 },
-    ["SupportCorps.PillCapsuleBag"]     = { Science =  5, Doctor =  5 },
-    ["SupportCorps.Vicodin"]            = { Science = 10, Doctor = 20 },
+    ["SupportCorps.ActivatedCharcoalPills"] = { Science = 10, Doctor = 20 },
+    ["SupportCorps.ColdaFluPills"]          = { Science = 10, Doctor = 20 },
+    ["SupportCorps.OrthopedicCast"]         = { Science =  5, Doctor = 25 },
+    ["SupportCorps.PillCapsule"]            = { Science =  5, Doctor =  5 },
+    ["SupportCorps.PillCapsuleBag"]         = { Science =  5, Doctor =  5 },
+    ["SupportCorps.Vicodin"]                = { Science = 10, Doctor = 20 },
 
     -- Cooking
-    ["SupportCorps.BottleCapper"]       = { Science = 10, Cooking = 10 },
-    ["SupportCorps.GlassFermenter"]     = { Science = 10, Cooking = 15 },
-    ["SupportCorps.PasturMachine"]      = { Science = 10, Cooking = 15 },
-    ["SupportCorps.YoghurtMaker"]       = { Science = 20, Cooking = 15 },
+    ["SupportCorps.BottleCapper"]           = { Science = 10, Cooking = 10 },
+    ["SupportCorps.GlassFermenter"]         = { Science = 10, Cooking = 15 },
+    ["SupportCorps.PasturMachine"]          = { Science = 10, Cooking = 15 },
+    ["SupportCorps.YoghurtMaker"]           = { Science = 20, Cooking = 15 },
 
     -- misc
-    ["SupportCorps.Handwarmer"]         = { Science = 10 },
-    ["SupportCorps.WeedGrinder"]        = { Science =  5, Farming = 5 },
+    ["SupportCorps.Handwarmer"]             = { Science = 10 },
+    ["SupportCorps.WeedGrinder"]            = { Science =  5, Farming = 5 },
 }})
